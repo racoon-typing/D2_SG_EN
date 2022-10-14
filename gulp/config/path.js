01b8caec1e4c469d4443cdb54779f9ce3db73b80
Daniil <eo18622@yandex.ru>
2022-10-14 15:54:31 +0300

    сервер

---
+++ gulp/config/path.js
@@ -0,0 +1,28 @@
+import * as nodePath from 'path';
+const rootFolder = nodePath.basename(nodePath.resolve());
+
+const buildFolder = './dist';
+const srcFolder = './source';
+
+export const path = {
+    build: {
+        css: `${buildFolder}/css`,
+        html: `${buildFolder}/`,
+        img: `${buildFolder}/img/`
+
+    },
+    src: {
+        less: `${srcFolder}/less/style.less`,
+        html: `${srcFolder}/*.html`,
+        img: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg}`
+    },
+    watch: {
+        html: `${srcFolder}/*.html`,
+        less: `${srcFolder}/**/*.less`
+    },
+    clean: buildFolder,
+    buildFolder: buildFolder,
+    srcFolder: srcFolder,
+    rootFolder: rootFolder,
+    ftp: ''
+}
---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+import gulp from "gulp";
+import { path } from "./gulp/config/path.js";
+
+global.app = {
+    path: path,
+    gulp: gulp,
+    plugins: plugins
+}
+
+// Импорт задач
+import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
+import { plugins } from "./gulp/config/plugins.js";
+import { server } from "./gulp/tasks/server.js";
+import { styles } from './gulp/tasks/less.js';
+import { copy } from './gulp/tasks/copy.js';
+
+// Наблюдатель за изменениями в файлах
+function watcher() {
+    gulp.watch(app.path.watch.html, html);
+    gulp.watch(app.path.watch.less, styles);
+}
+
+const mainTasks = gulp.parallel(copy, html, styles);
+
+// Сценарий выполнения
+const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
+
+// Выполнение сценария по умолчанию
+gulp.task('default', dev);
