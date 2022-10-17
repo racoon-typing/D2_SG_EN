8be6d4a32b8bd7028a58cc56271cd2095a2ffc4e
Daniil <eo18622@yandex.ru>
2022-10-17 14:14:45 +0300

    rkbr

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,8 @@
-let i = 1;
+let buttonNav = document.querySelector('.main-header__button-nav');
+let navNode = document.querySelector('.main-header__nav');
+
+buttonNav.addEventListener('click', function () {
+    console.log(клик);
+    navNode.classList.toggle('main-header__nav--close');
+    // if (navNode.classList.contains('main-header__nav--close'))
+});
