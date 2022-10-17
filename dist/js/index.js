0fb292882f39e29bc7ff0f94f6f06cca5598dfff
Daniil <eo18622@yandex.ru>
2022-10-17 17:31:19 +0300

    слайдер

--- dist/js/index.js
+++ dist/js/index.js
@@ -7,3 +7,23 @@ if (buttonNav) {
         navNode.classList.toggle('main-header__nav--close');
     });
 }
+
+// Инициализируем слайдер 
+new Swiper('.image-slider', {
+    // Стрелки
+    navigation: {
+        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next'
+    },
+    // Навигация
+    // Булеты, текущее положение
+    pagination: {
+        el: '.swiper-pagination',
+        // Буллеты
+        clickable: true,
+        // Динамические булеты
+        dynamicBullets: true,
+    },
+    // Отступ между слайдами
+    spaceBetween: 30,
+});
